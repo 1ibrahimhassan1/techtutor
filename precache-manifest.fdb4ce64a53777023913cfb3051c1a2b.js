3ac7e42b986fbc173a71d365bc026a51ea153a63
meme <1ibrahimhassan1@gmail.com>
2019-03-19 18:34:25 -0400

    Updates

---
+++ precache-manifest.fdb4ce64a53777023913cfb3051c1a2b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5d182da4f0dc984552f",
+    "url": "/techtutor/static/js/runtime~main.e58de6fe.js"
+  },
+  {
+    "revision": "f1c71215cb15754d0a5a",
+    "url": "/techtutor/static/js/main.a57bd762.chunk.js"
+  },
+  {
+    "revision": "dc012ac24166e9ed258a",
+    "url": "/techtutor/static/js/2.0d258afd.chunk.js"
+  },
+  {
+    "revision": "f1c71215cb15754d0a5a",
+    "url": "/techtutor/static/css/main.4402423c.chunk.css"
+  },
+  {
+    "revision": "5df41c8dbd99a198293fa56ce5ebe762",
+    "url": "/techtutor/index.html"
+  }
+];
